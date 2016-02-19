3da8109078a526e8381403aa6632d027cd5291ec
Alex <alex.sampley@gmail.com>
2016-02-19 15:57:45 -0700

    Added jumping

--- src/Monster's Kitchen/Assets/Scripts/PlayerController.cs
+++ src/Monster's Kitchen/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
     public int horizontalSpeed;
+    public int jumpSpeed;
+
+    private bool onGround;
 
 	// Use this for initialization
 	void Start () {
@@ -22,6 +25,27 @@ public class PlayerController : MonoBehaviour {
         { // move right
             horizVel = horizontalSpeed;
         }
+
+        if (Input.GetAxis("Vertical") > 0 && IsGrounded())
+        {
+            vertVel = jumpSpeed;
+            onGround = false;
+        }
         rigidbody2D.velocity = new Vector2(horizVel, vertVel);
+    }
+
+    public bool IsGrounded()
+    {
+        return onGround;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+
+        if (collision.transform.position.y < rigidbody2D.transform.position.y)
+        {
+            onGround = true;
+        }
     }
 }
