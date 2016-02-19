fa2eca3ac16e9bde985fe7af240c054fd8aa3e85
Alex <alex.sampley@gmail.com>
2016-02-19 14:42:59 -0700

    Added player controller script

---
+++ src/Monster's Kitchen/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public int horizontalSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rigidbody2D rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        float vertVel = rigidbody2D.velocity.y;
+        float horizVel = 0.0f;
+
+        if (Input.GetAxis("Horizontal") < -0.5)
+        { // move left
+            horizVel = -horizontalSpeed;
+        } else if (Input.GetAxis("Horizontal") > 0.5)
+        { // move right
+            horizVel = horizontalSpeed;
+        }
+        rigidbody2D.velocity = new Vector2(-horizontalSpeed, rigidbody2D.velocity.y);
+    }
+}
