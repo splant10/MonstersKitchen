e8a6b15931365831513031ca30d69d37e74e7d2d
Alex <alex.sampley@gmail.com>
2016-02-19 17:03:19 -0700

    Fixed jump

--- src/Monster's Kitchen/Assets/Scripts/PlayerController.cs
+++ src/Monster's Kitchen/Assets/Scripts/PlayerController.cs
@@ -7,8 +7,6 @@ public class PlayerController : MonoBehaviour {
 
     public Transform bottom;
 
-    private bool onGround;
-
 	// Use this for initialization
 	void Start () {
 
@@ -31,23 +29,13 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetAxis("Vertical") > 0 && IsGrounded())
         {
             vertVel = jumpSpeed;
-            onGround = false;
         }
         rigidbody2D.velocity = new Vector2(horizVel, vertVel);
     }
 
     public bool IsGrounded()
     {
-        return Physics2D.Linecast(bottom.position, (Vector2)bottom.position + (0.1f * Vector2.down));
-    }
+        return Physics2D.Linecast(bottom.position, (Vector2)bottom.position + (0.1f * Vector2.down), LayerMask.GetMask("Ground"));
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        Rigidbody2D rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
-
-        if (other.gameObject.layer.ToString().Equals("Ground"))
-        {
-            onGround = true;
-        }
     }
 }
