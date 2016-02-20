7b75138c4296cb8ebb6c24e318aaa01361fef1f4
Alex <alex.sampley@gmail.com>
2016-02-19 18:38:03 -0700

    Added cauldron object and script

---
+++ src/Monster's Kitchen/Assets/Scripts/Cauldron.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Cauldron : MonoBehaviour, Interactable {
+    public void Interact(GameObject interactor)
+    {
+        Debug.Log("Yo, we haven't implemented cooking yet.");
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
