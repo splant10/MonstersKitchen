5f219bcd6b6f3d1803d7471bbef8683bc6e7af03
Alex <alex.sampley@gmail.com>
2016-02-21 10:27:03 -0700

    Added spider spawners

---
+++ src/Monster's Kitchen/Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MonsterSpawner : MonoBehaviour, DoorListener {
+    public Monster toSpawn;
+    public Door[] doorsToCauseSpawn;
+    private Monster monster;
+
+    void Start()
+    {
+        foreach (Door door in doorsToCauseSpawn)
+        {
+            door.AddListener(this);
+        }
+    }
+
+    public bool Spawn()
+    {
+        if (monster == null)
+        {
+            monster = Instantiate(toSpawn);
+            monster.transform.position = gameObject.transform.position;
+            monster.transform.SetParent(gameObject.transform);
+            return true;
+        }
+        return false;
+    }
+
+    public void OnDoor(Door door)
+    {
+        Spawn();
+    }
+}
