45f0b4073ba84ce0458249093e0bc7d5d9bd4047
Alex <alex.sampley@gmail.com>
2016-02-20 19:25:10 -0700

    Added Attackable interface, and made Monster script

---
+++ src/Monster's Kitchen/Assets/Scripts/Monster.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Monster : MonoBehaviour, Attackable, Interactable {
+    private bool isAlive;
+    public Ingredient.ID ingredient;
+
+    public void Attack(GameObject attacker)
+    {
+        isAlive = false;
+    }
+
+    public void Interact(GameObject interactor)
+    {
+        if (!isAlive)
+        {
+            interactor.GetComponent<PlayerController>().inventory.ingredients.Add(ingredient);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        isAlive = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
