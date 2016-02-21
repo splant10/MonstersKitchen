0534f3f359c24055ff698d1856d0bb7724123d9b
Cameron Murray <joannehomik@uws105591usr.uws.ualberta.ca>
2016-02-21 12:10:09 -0700

    changed order times

--- src/Monster's Kitchen/Assets/Scripts/OrderCreater.cs
+++ src/Monster's Kitchen/Assets/Scripts/OrderCreater.cs
@@ -20,27 +20,27 @@ public class OrderCreater : MonoBehaviour {
 		List<Ingredient.ID> ingredientIDs;
 
 		ingredientIDs = new List<Ingredient.ID>(new Ingredient.ID[] { Ingredient.ID.FLOUR, Ingredient.ID.SUGAR, Ingredient.ID.EGGS});
-		allRecipes[0] = new Recipe(0, "CauldronCakes", 30, ingredientIDs);
+		allRecipes[0] = new Recipe(0, "CauldronCakes", 15, ingredientIDs);
 
 		ingredientIDs = new List<Ingredient.ID>(new Ingredient.ID[] { Ingredient.ID.SUGAR, Ingredient.ID.EGGS});
-        allRecipes[1] = new Recipe(1, "Murangueei", 30, ingredientIDs);
+        allRecipes[1] = new Recipe(1, "Murangueei", 15, ingredientIDs);
 
 		ingredientIDs = new List<Ingredient.ID> (new Ingredient.ID[] {
 			Ingredient.ID.OGREEGG,
 			Ingredient.ID.EGGS,
 			Ingredient.ID.FLOUR
 		});
-		allRecipes [2] = new Recipe (2, "OgreEgg Quiche", 30, ingredientIDs);
+		allRecipes [2] = new Recipe (2, "OgreEgg Quiche", 45, ingredientIDs);
 
 		ingredientIDs = new List<Ingredient.ID> (new Ingredient.ID[] {
 			Ingredient.ID.SUGAR,
 			Ingredient.ID.STARFISHBLOOD,
 			Ingredient.ID.SPIDEREYE
 		});
-		allRecipes [3] = new Recipe (3, "Blood Jello", 30, ingredientIDs);
+		allRecipes [3] = new Recipe (3, "Blood Jello", 35, ingredientIDs);
 
 		ingredientIDs = new List<Ingredient.ID>(new Ingredient.ID[] { Ingredient.ID.FLOUR, Ingredient.ID.SPIDEREYE, Ingredient.ID.EGGS});
-		allRecipes [4] = new Recipe (4, "Spider Eye pudding", 30, ingredientIDs);
+		allRecipes [4] = new Recipe (4, "Spider Eye pudding", 20, ingredientIDs);
 
 	}
 
